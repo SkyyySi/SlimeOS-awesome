77cb6120f84dd6e1c81b7b908aff65fb9b956d71
SkyyySi <rasti.b@protonmail.com>
2022-04-29 21:34:40 +0200

    added a few more improvements to gitamp | +%d/%m/%Y (%a) %T [%Z]

--- src/helper.sh
+++ src/helper.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 # This is a helper script, intended to be sourced. It provides some usefull
 # aliases, functions and environment settings.
+# This file should be loadable by any POSIX shell. For zsh, you can use
+# the following to ensure compatibility:
+#emulate sh -c 'source "[...]/src/helper.sh"'
 
 AWESOME_CONFIG_DIR="$(dirname "$(basename "$0")")"
 export AWESOME_CONFIG_DIR
@@ -11,7 +14,12 @@ gitamp() {
 		return 1
     fi
 
+	echo " ==> Adding files <== "
 	git add "${AWESOME_CONFIG_DIR:-.}"
+	echo
+	echo " ==> Commiting changes <== "
 	git commit -m "${1} | +%d/%m/%Y (%a) %T [%Z]"
+	echo
+	echo " ==> Pushing changes (to $(git branch --show-current) branch) <== "
 	git push
 }
